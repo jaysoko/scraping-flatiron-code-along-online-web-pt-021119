6a3cf651d1f8331b880712e058419ab9687cba9d
Jay Sokolowski <jaysoko@gmail.com>
2019-03-31 20:43:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,18 @@ class Scraper
       end
     end
   end
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+  end
+
+  def make_courses
+  end
+
+  
   
 end
 
